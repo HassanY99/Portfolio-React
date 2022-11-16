9e2732e01b3a438dd5fb81e28b4d9204d0e5e5a9
Hassan Yusuf <hassanyousuf1999@gmail.com>
2022-11-16 12:47:42 -0500

    added name, info and icons on the home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,7 @@
 import useMediaQuery from "../hooks/useMediaQuery";
+import { motion } from "framer-motion";
+import AnchorLink from "react-anchor-link-smooth-scroll";
+import SocialMediaIcons from "../icons/SocialMediaIcons";
 
 const Home = ({ setSelectedPage }) => {
     const isAboveLarge = useMediaQuery("(min-width: 1060px)");
@@ -16,7 +19,7 @@ const Home = ({ setSelectedPage }) => {
             >
               <img
                 alt="profile"
-                className="hover:filter hover:saturate-200 transition duration-500 z-10 w-full max-w-[400px] md:max-w-[600px] rounded-full"
+                className="hover:filter hover:scale-90 transition duration-500 z-10 w-full max-w-[400px] md:max-w-[600px] rounded-full object-contain"
                 src="assets/hydev.PNG"
               />
             </div>
@@ -27,6 +30,80 @@ const Home = ({ setSelectedPage }) => {
               src="assets/hydev.PNG"
             />
           )}
+        </div>
+
+        {/* MAIN TEXT */}
+      <div className="z-30 basis-2/5 mt-12 md:mt-32">
+        {/* HEADINGS */}
+        <motion.div
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          transition={{ duration: 0.5 }}
+          variants={{
+            hidden: { opacity: 0, x: -50 },
+            visible: { opacity: 1, x: 0 },
+          }}
+        >
+          <p className="text-6xl font-playfair z-10 text-center md:text-start">
+            Hassan {""}
+            <span
+              className="xs:relative xs:text-deep-blue xs:font-semibold z-20 xs:before:content-brush
+              before:absolute before:-left-[25px] before:-top-[70px] before:z-[-1]"
+            >
+              Yusuf
+            </span>
+          </p>
+
+          <p className="mt-10 mb-7 text-sm text-center md:text-start">
+            I am a Full Stack Web developer from NYC. I love to code and solve problems. 
+          </p>
+        </motion.div>
+
+        {/* CALL TO ACTIONS */}
+        <motion.div
+          className="flex mt-5 justify-center md:justify-start"
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          transition={{ delay: 0.2, duration: 0.5 }}
+          variants={{
+            hidden: { opacity: 0, x: -50 },
+            visible: { opacity: 1, x: 0 },
+          }}
+        >
+          <AnchorLink
+            className="bg-gradient-to-r from-teal-500 to-indigo-500 hover:from-pink-500 hover:to-orange-500 text-deep-blue rounded-sm py-3 px-7 font-semibold
+             hover:text-white transition duration-500"
+            onClick={() => setSelectedPage("contact")}
+            href="#contact"
+          >
+            Contact Me
+          </AnchorLink>
+          <AnchorLink
+            className="rounded-r-sm bg-gradient-rainblue py-0.5 pr-0.5"
+            onClick={() => setSelectedPage("contact")}
+            href="#contact"
+          >
+            <div className="bg-deep-blue hover:text-teal-500 transition duration-500 w-full h-full flex items-center justify-center px-10 font-playfair">
+              Let's talk.
+            </div>
+          </AnchorLink>
+        </motion.div>
+
+        <motion.div
+          className="flex mt-5 justify-center md:justify-start"
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          transition={{ delay: 0.4, duration: 0.5 }}
+          variants={{
+            hidden: { opacity: 0, x: -50 },
+            visible: { opacity: 1, x: 0 },
+          }}
+        >
+          <SocialMediaIcons />
+        </motion.div>
         </div>
       </section>
   );
